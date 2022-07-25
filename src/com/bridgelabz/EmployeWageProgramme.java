7304add133f4059cbeee9c74b75ee3d09e15d4ec
piyushkolhe94 <piyushkolhe94gmail.com>
2022-07-25 17:56:28 +0530

    Added Half DAy wage logic

--- src/com/bridgelabz/EmployeWageProgramme.java
+++ src/com/bridgelabz/EmployeWageProgramme.java
@@ -4,17 +4,28 @@ public class EmployeWageProgramme {
 
 	static final int WAGEPERHOUR = 20;
 	static final int FULLDAYHOURS = 8;
+	static final int HALFDAYHOURS = 4;
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation");
 
-		int present = 1;
-		int value = (int) ((Math.random() * 10) % 2);
-		if (value == present) {
-			System.out.println("Employee is present");
-			int employeewage = WAGEPERHOUR * FULLDAYHOURS;
+		int fulldaypresent = 1;
+		int halfdaypresent = 2;
+		int value = (int) ((Math.random() * 10) % 3);
+
+		if (value == fulldaypresent) {
+			System.out.println("Employee is fulldaypresent");
+            int employeewage = WAGEPERHOUR * FULLDAYHOURS;
 			System.out.println("Employeewage is: " + employeewage);
-		} else
+
+		} else if (value == halfdaypresent) {
+			System.out.println("Employee is halfdaypresent");
+            int employeewage = WAGEPERHOUR * HALFDAYHOURS;
+			System.out.println("Employeewage is: " + employeewage);
+			
+       } else {
+    	   
 			System.out.println("Employee is Absent+ No Wage");
+		}
 	}
 }
